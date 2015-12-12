f283804396aeb810b2b6b2fd261ee3fd18fb7ac3
Amr Alaa <n1amr1@gmail.com>
2015-12-13 00:37:06 +0200

    change alu_op to alu_control

--- alu.v
+++ alu.v
@@ -1,10 +1,10 @@
-module ALU(a, b, alu_op, result, zero);
+module ALU(a, b, alu_control, result, zero);
   input [31:0] a, b;
-  input [1:0] alu_op;
+  input [1:0] alu_control;
   output reg [31:0] result;
   output reg zero;
 
-  always @ (a or b or alu_op) begin
+  always @ (a or b or alu_control) begin
     case(alu_op)
       2'b00:
         result <= a + b;
@@ -13,7 +13,7 @@ module ALU(a, b, alu_op, result, zero);
       2'b10:
         result <= a & b;
       2'b11:
-        result <= a | b;
+        result <= ~(a | b);
     endcase
   end
 endmodule
@@ -21,18 +21,18 @@ endmodule
 module ALU_testbench();
   reg [31:0] a, b;
   wire [31:0] result;
-  reg [1:0] alu_op;
+  reg [1:0] alu_control;
   wire zero;
 
-  ALU alu(a, b, alu_op, result, zero);
+  ALU alu(a, b, alu_control, result, zero);
 
   initial begin
     a <= 1;
     b <= 3;
 
-    alu_op <= 2'b00;
-    #5 alu_op <= 2'b01;
-    #5 alu_op <= 2'b10;
-    #5 alu_op <= 2'b11;
+    alu_control <= 2'b00;
+    #5 alu_control <= 2'b01;
+    #5 alu_control <= 2'b10;
+    #5 alu_control <= 2'b11;
   end
 endmodule
