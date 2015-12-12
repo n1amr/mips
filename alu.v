1350a460015ff8423563e5e52516bc5c4d820a63
Amr Alaa <n1amr1@gmail.com>
2015-12-13 01:00:46 +0200

    add sll to alu

--- alu.v
+++ alu.v
@@ -1,19 +1,32 @@
-module ALU(a, b, alu_control, result, zero);
+module ALU(a, b, alu_control, shamt, result, zero);
   input [31:0] a, b;
-  input [1:0] alu_control;
+  input [3:0] alu_control;
+  input [4:0] shamt;
   output reg [31:0] result;
   output reg zero;
 
-  always @ (a or b or alu_control) begin
-    case(alu_op)
-      2'b00:
+  parameter [3:0]
+    ADD = 4'b0010,
+    SUBTRACT = 4'b0110,
+    AND = 4'b0000,
+    NOR = 4'b0001,
+    SLT = 4'b0111,
+    SLL = 4'b1000;
+
+  always @ (a or b or alu_control or shamt) begin
+    case(alu_control)
+      ADD:
         result <= a + b;
-      2'b01:
+      SUBTRACT:
         result <= a - b;
-      2'b10:
+      AND:
         result <= a & b;
-      2'b11:
+      NOR:
         result <= ~(a | b);
+      SLT:
+        result <= a < b;
+      SLL:
+        result <= a << shamt;
     endcase
   end
 endmodule
@@ -21,18 +34,31 @@ endmodule
 module ALU_testbench();
   reg [31:0] a, b;
   wire [31:0] result;
-  reg [1:0] alu_control;
+  reg [3:0] alu_control;
+  reg [4:0] shamt;
   wire zero;
 
-  ALU alu(a, b, alu_control, result, zero);
+  parameter [3:0]
+    ADD = 4'b0010,
+    SUBTRACT = 4'b0110,
+    AND = 4'b0000,
+    NOR = 4'b0001,
+    SLT = 4'b0111,
+    SLL = 4'b1000;
+
+  ALU alu(a, b, alu_control, shamt, result, zero);
 
   initial begin
-    a <= 1;
-    b <= 3;
+    a <= 4;
+    b <= 1;
 
-    alu_control <= 2'b00;
-    #5 alu_control <= 2'b01;
-    #5 alu_control <= 2'b10;
-    #5 alu_control <= 2'b11;
+    alu_control = ADD;
+    #5 alu_control = SUBTRACT;
+    #5 alu_control = AND;
+    #5 alu_control = NOR;
+    #5 alu_control = SLL;
+    #5 shamt = 3;
+    #5 shamt = 4;
+    #5 shamt = 5;
   end
 endmodule
