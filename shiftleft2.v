d95dfa33d661ca6e33958042c7d87fe22f50dd0a
Amr Alaa <n1amr1@gmail.com>
2015-12-13 21:11:47 +0200

    replace multiplication by left shifting

--- shiftleft2.v
+++ shiftleft2.v
@@ -1,23 +1,21 @@
-module ShiftLeft (in , out );
-input  [31:0]in ;
-output [31:0]out ;
+module ShiftLeft2(in, out);
+  input [31:0] in;
+  output [31:0] out;
 
-assign out = in * 4 ;
-
+  assign out = in << 2 ;
 endmodule
-module shiftleft_testbench();
- reg [31:0]a;
- wire [31:0] b;
 
-  ShiftLeft try (a, b);
+module ShiftLeft2_testbench();
+  reg [31:0] a;
+  wire [31:0] b;
+
+  ShiftLeft2 try (a, b);
 
   initial begin
     a <= 4;
-    #5  a= 6;
+    #5 a = 6;
     #5 a = 8;
     #5 a = 81;
     #5 a = -82;
   end
 endmodule
-
-
