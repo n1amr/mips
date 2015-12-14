e0477dadf7ed94f6701678909a45f1a314de26e3
Amr Alaa <n1amr1@gmail.com>
2015-12-15 00:40:16 +0200

    make register file writing synchronous

--- register-file.v
+++ register-file.v
@@ -1,7 +1,7 @@
-module RegisterFile(read_reg1, read_reg2, write_reg, write_data, RegWrite, read_data1, read_data2);
+module RegisterFile(read_reg1, read_reg2, write_reg, write_data, RegWrite, clk, read_data1, read_data2);
   input [5:0] read_reg1, read_reg2, write_reg;
   input [31:0] write_data;
-  input RegWrite;
+  input RegWrite, clk;
   output reg [31:0] read_data1, read_data2;
 
   reg [31:0] registers [0:31];
@@ -14,8 +14,8 @@ module RegisterFile(read_reg1, read_reg2, write_reg, write_data, RegWrite, read_
     #100 read_data2 = registers[read_reg2];
   end
 
-  always @(write_reg or write_data or RegWrite) begin
-    if(RegWrite) begin
+  always @(posedge clk) begin
+    if(RegWrite && write_reg != 5'b00000) begin
       #100 registers[write_reg] = write_data;
     end
   end
@@ -32,25 +32,36 @@ endmodule
 module RegisterFile_testbench;
   reg [5:0] read_reg1, read_reg2, write_reg;
   reg [31:0] write_data;
-  reg RegWrite;
+  reg RegWrite, clk;
   wire [31:0] read_data1, read_data2;
 
-  RegisterFile regs(read_reg1, read_reg2, write_reg, write_data, RegWrite, read_data1, read_data2);
+  RegisterFile regs(read_reg1, read_reg2, write_reg, write_data, RegWrite, clk, read_data1, read_data2);
+
+  initial begin
+    clk <= 0;
+    forever #500 clk <= ~clk;
+  end
 
   initial begin
     read_reg1 <= 1;
-    read_reg2 <= 2;
+    read_reg2 <= 3;
+    write_reg <= 3;
 
     RegWrite = 0;
-    write_reg <= 3;
-    write_data <= 32'habcdef12;
 
-    #500 read_reg1 = 2;
-    read_reg1 = 3;
+    #1000
+    #100 read_reg1 = 2;
 
-    #500 RegWrite = 1;
+    #100 write_data <= 32'habcdef12;
+    #100 RegWrite <= 1;
 
-    #500 RegWrite <= 0;
-    write_data <= 0;
+    #1000 RegWrite <= 0;
+
+    #500 $finish;
+  end
+
+  initial begin
+    $dumpfile("RegisterFile_testbench.vcd");
+    $dumpvars(0, RegisterFile_testbench);
   end
 endmodule
