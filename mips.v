9d54e4b5d3a5b188520d5ef14766232bdedcfb56
Amr Alaa <n1amr1@gmail.com>
2015-12-20 18:39:49 +0200

    add regdstmux

--- mips.v
+++ mips.v
@@ -7,18 +7,28 @@ module MIPS;
 
   wire [31:0] pc, next_pc;
 
-  PC pc_module(.in(next_pc), .clk(clk), .out(pc));
-  Adder adder_module(.a(pc), .b(4), .result(next_pc));
+  PC pc_module(next_pc, clk, pc);
+  Adder adder_module(pc, 4, next_pc);
 
   wire [31:0] instruction;
-  InstructionMemory instruction_memory_module(.read_address(pc), .instruction(instruction));
+  InstructionMemory instruction_memory_module(pc, instruction);
 
-  wire [6:0] opcode, funct;
-  wire [5:0] rs, rt, rd, shamt;
+  wire [5:0] opcode, funct;
+  wire [4:0] rs, rt, rd, shamt;
   wire [15:0] shift;
   wire [25:0] jump_address;
   
-  Decoder decoder(.instruction(instruction), .opcode(opcode), .rs(rs), .rt(rt), .rd(rd), .shamt(shamt), .funct(funct), .shift(shift), .jump_address(jump_address));
+  Decoder decoder(instruction, opcode, rs, rt, rd, shamt, funct, shift, jump_address);
 
+  wire [4:0] write_reg;
+  RegDstMux reg_dst_mux(write_reg, RegDst, rt, rd);
 
+endmodule
+
+module RegDstMux(out, RegDst, rt, rd);
+  input RegDst;
+  input [4:0] rt, rd;
+  output wire [4:0] out;
+
+  assign out = RegDst? rd : rt;
 endmodule
