77461818380258e1879cb1901ce6b7ca7211cca2
Amr Alaa <n1amr1@gmail.com>
2015-12-20 18:51:05 +0200

    [Fix] change register file address to 5 bits

--- register-file.v
+++ register-file.v
@@ -1,5 +1,5 @@
 module RegisterFile(read_reg1, read_reg2, write_reg, write_data, RegWrite, clk, read_data1, read_data2);
-  input [5:0] read_reg1, read_reg2, write_reg;
+  input [4:0] read_reg1, read_reg2, write_reg;
   input [31:0] write_data;
   input RegWrite, clk;
   output reg [31:0] read_data1, read_data2;
