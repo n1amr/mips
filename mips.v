08f5cf83984d1fa95fc886a187b6a56980d1e125
Amr Alaa <n1amr1@gmail.com>
2015-12-20 18:07:31 +0200

    add decoder to mips module

--- mips.v
+++ mips.v
@@ -13,4 +13,12 @@ module MIPS;
   wire [31:0] instruction;
   InstructionMemory instruction_memory_module(.read_address(pc), .instruction(instruction));
 
+  wire [6:0] opcode, funct;
+  wire [5:0] rs, rt, rd, shamt;
+  wire [15:0] shift;
+  wire [25:0] jump_address;
+  
+  Decoder decoder(.instruction(instruction), .opcode(opcode), .rs(rs), .rt(rt), .rd(rd), .shamt(shamt), .funct(funct), .shift(shift), .jump_address(jump_address));
+
+
 endmodule
