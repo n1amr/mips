4b9b493f7f3d29932868304b337a5df2cd863836
MohamedA-M-Hassan <anehmr0@gmail.com>
2015-12-22 10:46:16 +0200

    signextend to handle the add & addi

--- signextend.v
+++ signextend.v
@@ -1,23 +1,32 @@
-module Sign_extend (in , out );
+module Sign_extend (in , sign , out );
   input  [15:0]in ;
-  output [31:0]out ;
-
-  assign out = { {16{in[15]}}, in };
+  input sign;
+  output reg [31:0]out ;
+	
+	always @ (sign or in )
+  begin
+      if ( sign == 0)  // to handle the "and" "andi" cases
+     	out = {16'b0000_0000_0000_0000 , in};
+	
+	else if (sign == 1)  // the other inst that need the sign extend
+     	  out = { {16{in[15]}}, in };
+ end	
+  
 endmodule
 
 module SignExtend_testbench();
-  reg [15:0]a;
-  wire [31:0] b;
+  reg [15:0] in;
+  reg sign ;
+  wire [31:0] out;
 
-  Sign_extend try (a, b);
+  Sign_extend try ( in , sign,out );
 
   initial begin
-    a <= 4;
-    #5  a= 6;
-    #5 a = 8;
-    #5 a = 81;
-    #5 a = -82;
+    in <= 16'b0000_0000_0000_0100;
+    sign <= 0 ; 	
+    #5 in <= 16'b 1000_1110_1111_0000;
+    #5 sign <= 1;
+    #5 in <= 16'b  1000_1110_1111_0000 ;
+     #5 in <= 16'b0000_0000_0000_0100;
   end
 endmodule
-
-
