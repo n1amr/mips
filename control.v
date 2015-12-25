27e57ede80fec7e6482c5cbd23c1e158df975ef2
Amr Alaa <n1amr1@gmail.com>
2015-12-25 17:08:40 +0200

    undo supporting new load/store instructions

--- control.v
+++ control.v
@@ -1,25 +1,19 @@
-module Control(opcode, RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite, Jump, MemDataSize, MemDataSign);
+module Control(opcode, RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite, Jump);
   input [5:0] opcode;
-  output wire RegWrite, Branch, ALUSrc, MemRead, MemWrite, Jump, MemDataSign;
-  output wire [1:0] ALUOp, RegDst, MemtoReg, MemDataSize;
+  output wire RegWrite, Branch, ALUSrc, MemRead, MemWrite, Jump;
+  output wire [1:0] ALUOp, RegDst, MemtoReg;
   
   parameter [5:0]
     RFORMAT = 6'd0,
     ADDI = 6'd8,
     ANDI = 6'd12,
     LW = 6'd35,
-    LB = 6'b100000,
-    LBU = 6'b100100,
-    LH = 6'b100001,
-    LHU = 6'b100101,
     SW = 6'd43,
-    SB = 6'b101000,
-    SH = 6'b101001,
     BEQ = 6'd5,
     JAL = 6'd3;
 
-  wire LOAD = (opcode == LW || opcode == LB || opcode == LBU || opcode == LH || opcode == LHU);
-  wire STORE = (opcode == SW || opcode == SB || opcode == SH);
+  wire LOAD = (opcode == LW);
+  wire STORE = (opcode == SW);
 
   assign #10 Jump = (opcode == JAL);
   assign #10 Branch = (opcode == BEQ);
@@ -33,18 +27,15 @@ module Control(opcode, RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSr
   assign #10 RegWrite = (opcode == RFORMAT || opcode == ADDI || opcode == ANDI || LOAD || opcode == JAL);
   assign #10 ALUOp[1] = (opcode == RFORMAT || opcode == ANDI);
   assign #10 ALUOp[0] = (opcode == ANDI || opcode == BEQ);
-  assign #10 MemDataSign = (opcode == LW || opcode == LB || opcode == LH || opcode == SW || opcode == SB || opcode == SH);
-  assign #10 MemDataSize[1] = (opcode == LW || opcode == LH || opcode == LHU || opcode == SW || opcode == SH);
-  assign #10 MemDataSize[0] = (opcode == LW || opcode == LB || opcode == LBU || opcode == SW || opcode == SB);
 
 endmodule
 
 module Control_testbench();
   reg [5:0] opcode;
-  wire RegWrite, Branch, ALUSrc, MemRead, MemWrite, Jump, MemDataSign;
-  wire [1:0] ALUOp, RegDst, MemtoReg, MemDataSize;
+  wire RegWrite, Branch, ALUSrc, MemRead, MemWrite, Jump;
+  wire [1:0] ALUOp, RegDst, MemtoReg;
 
-  Control control(opcode, RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite, Jump, MemDataSize, MemDataSign);
+  Control control(opcode, RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite, Jump);
 
   parameter [5:0]
     RFORMAT = 6'd0,
@@ -64,11 +55,6 @@ module Control_testbench();
     # 10 opcode = BEQ;
     # 10 opcode = JAL;
     # 10 $finish;
-  end
-
-  initial begin
-    $dumpfile("Control_testbench.vcd");
-    $dumpvars(0, Control_testbench);
   end
 endmodule
 
--- data_memory.v
+++ data_memory.v
@@ -1,9 +1,7 @@
-module DataMemory(address, write_data, MemRead, MemWrite, MemDataSize, MemDataSign, clk, read_data);
+module DataMemory(address, write_data, MemRead, MemWrite, clk, read_data);
   input [31:0] address, write_data;
   input MemRead, MemWrite, clk;
   output wire [31:0] read_data;
-  input MemDataSign;
-  input [1:0] MemDataSize;
 
   reg [7:0] mem [0:4096];
 
@@ -12,22 +10,12 @@ module DataMemory(address, write_data, MemRead, MemWrite, MemDataSize, MemDataSi
     HALFWORD = 2'b10,
     BYTE = 2'b01;
 
-  wire [31:0] signed_data;
-  wire [31:0] unsigned_data;
-
-  assign signed_data = (MemDataSize == WORD)? {mem[address], mem[address + 1], mem[address + 2], mem[address + 3]} : (MemDataSize == HALFWORD)? {{16{mem[address][7]}}, mem[address], mem[address + 1]} : (MemDataSize == BYTE)? {{24{mem[address][7]}},mem[address]} : 32'bx;
-  assign unsigned_data = (MemDataSize == WORD)? {mem[address], mem[address + 1], mem[address + 2], mem[address + 3]} : (MemDataSize == HALFWORD)? {{16{1'b0}}, mem[address], mem[address + 1]} : (MemDataSize == BYTE)? {{24{1'b0}},mem[address]} : 32'bx ;
-  assign #200 read_data = (MemRead)? ((MemDataSign == 1'b1)? signed_data : unsigned_data) : 32'bz;
+  assign #200 read_data = (MemRead)? {mem[address], mem[address + 1], mem[address + 2], mem[address + 3]} : 32'bz;
   
 
   always @ (negedge clk) begin
     if(MemWrite) begin
-      if(MemDataSize == WORD)
-        #100 {mem[address], mem[address + 1], mem[address + 2], mem[address + 3]}= write_data;
-      else if(MemDataSize == HALFWORD)
-        #100 {mem[address], mem[address + 1]}= write_data[15:0];
-      else if(MemDataSize == BYTE)
-        #100 mem[address]= write_data[7:0];
+      #100 {mem[address], mem[address + 1], mem[address + 2], mem[address + 3]} = write_data;
     end
   end
 
@@ -39,68 +27,35 @@ endmodule
 module DataMemory_testbench();
   reg [31:0] address, write_data;
   reg MemRead, MemWrite, clk;
-  reg MemDataSign;
-  reg [1:0] MemDataSize;
   wire [31:0] read_data;
 
-  DataMemory mem(address, write_data, MemRead, MemWrite, MemDataSize, MemDataSign, clk, read_data);
+  DataMemory mem(address, write_data, MemRead, MemWrite, clk, read_data);
 
   initial begin
     clk = 0;
-    forever #50 clk = ~clk;
+    forever #500 clk = ~clk;
   end
 
   integer i;
   initial begin
-    MemDataSize = 2'b01;
-    MemDataSign = 0;
     address = 16;
     MemRead = 1;
     MemWrite = 0;
 
     #200
 
-    MemDataSign = 1;
     MemWrite = 1;
     MemRead = 1;
-    MemDataSize <= 2'b11;
     address <= 4;
     write_data <= 32'h01234567;
 
     #200
-
-    MemDataSize = 2'b10;
-    address <= 10;
-    write_data <= 32'h00004567;
-
-    #200
-
-    MemDataSize = 2'b01;
-    address <= 13;
-    write_data <= 32'h00000067;
-
-    #200    
     
     MemRead = 1;
     MemWrite = 0;
 
-    MemDataSize = 2'b11;
     for(i = 0; i < 4; i = i + 1) begin
       #100 address <= i << 2;
-    end
-
-    #100
-
-    MemDataSize = 2'b10;
-    for(i = 0; i < 8; i = i + 1) begin
-      #100 address <= i << 1;
-    end
-
-    #100
-
-    MemDataSize = 2'b01;
-    for(i = 0; i < 16; i = i + 1) begin
-      #100 address <= i;
     end
 
     #200 $finish;
